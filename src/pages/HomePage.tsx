06f1dbac01a3ddede6664377f97f7c61dec3978c
Andrei Komolov <andreykomolov@gmail.com>
2022-08-02 10:38:36 -0400

    Added RepoCard component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { RepoCard } from "../components/RepoCard";
 import { useDebounce } from "../hooks/debounce";
 import {
   useLazyGetUserReposQuery,
@@ -59,7 +60,7 @@ export function HomePage() {
           {areReposLoading && (
             <p className="text-center">Repos are loading...</p>
           )}
-          { repos?.map(repo => <p>{repo.url}</p>)}
+          { repos?.map(repo => <RepoCard repo={ repo } key={ repo.id } />)}
         </div>
       </div>
     </div>
